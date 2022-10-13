9110fd84e28a8b6988343d4ba35211856ea124b6
MarinaGlad <musik20_87@mail.ru>
2022-10-13 14:42:39 +0300

    first commit

--- Test/Program.cs
+++ Test/Program.cs
@@ -4,3 +4,27 @@
 либо задать на старте выполнения алгоритма.
 При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 */
+
+int size = 3;
+string[] arrayStrings = new string[size];
+int pos = 0;
+for (int i = 0; i < size; i++)
+{     
+ Console.WriteLine("Введите элементы ");
+ string element = Convert.ToString(Console.ReadLine());
+ if (element.Length <= 3)
+  {
+    arrayStrings[pos] = element;
+    pos++;
+  }
+}
+Console.WriteLine();
+PrintArray(arrayStrings);
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
